a646c4601ee9bd6411d205abfebfdff4618ae838
Plasmaa <zarifpious11@gmail.com>
2025-12-13 00:24:35 +0600

    Deploy with imported images

---
+++ _next/static/chunks/2ce1229268fe4ddd.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,18566,(t,e,o)=>{e.exports=t.r(76562)},63687,t=>{"use strict";var e=t.i(71645),o=t.i(18566);function u(){let t=(0,o.usePathname)();return(0,e.useEffect)(()=>{window.scrollTo(0,0)},[t]),null}t.s(["default",()=>u])}]);
